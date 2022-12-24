46d619fbffd7205fe07ff9305c69f083115c4cdc
Michael Chiang <michael.chiang.mc5@gmail.com>
2022-12-23 23:13:27 -0800

    added global error handler, page not found error to backend

--- src/server/authRouter.js
+++ src/server/authRouter.js
@@ -1,3 +1,4 @@
+const { createNextState } = require('@reduxjs/toolkit');
 const express = require('express');
 const router = express.Router();
 require('./auth.js');
@@ -9,8 +10,14 @@ router.get('/user', (req, res) => {
   res.status(200).json(req.user ? req.user : null);
 });
 
+// When client accesses endpoint /google, backend will do request to google
+// authentication server. You need to enable google oauth authentication and
+// set up client ID /secret. In addition, you must set callback url to
+// /google/callback
 router.get('/google', passport.authenticate('google', { scope: ['email'] }));
 
+// Callback url. While setting up oauth authentication, you must set callback
+// url to /google/callback
 router.get(
   '/google/callback',
   passport.authenticate('google', {
@@ -22,7 +29,7 @@ router.get(
 );
 
 router.get('/failure', (request, response) => {
-  response.send('authentication failed...');
+  response.status(500).send('authentication failed...');
 });
 // TODO: add comments about login, logout being set by passport
 // https://www.passportjs.org/concepts/authentication/login/
--- src/server/server.js
+++ src/server/server.js
@@ -42,14 +42,27 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.status(200).redirect('http://localhost:5000/');
+  return res.status(200).redirect('http://localhost:5000/');
 });
 
 // routers
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 
+// page not fuound
+app.use('*', (req, res) => {
+  return res.status(404).send('404 not found');
+});
+
 // global error handler
+app.use((err, req, res, next) => {
+  const errTemplate = {
+    message: 'unknown error occured',
+    status: 500,
+    location: 'unknown location',
+  };
+  return res.status(errObj.status).json(Object.assign(errTemplate, err));
+});
 
 // start server
 app.listen(PORT, () => {
