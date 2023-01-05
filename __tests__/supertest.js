7a2c4289fb17575812b14008848ff5a5f50364b1
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-05 12:27:35 -0800

    added non-functional supertest

---
+++ __tests__/supertest.js
@@ -0,0 +1,17 @@
+const { expectedError } = require('@babel/core/lib/errors/rewrite-stack-trace');
+const request = require('supertest');
+
+describe('Route integration', () => {
+  it('asdf', () => {
+    return expect(1).toEqual(1);
+  });
+  it('asdf', () => {
+    return expect(1).toEqual(1);
+  });
+});
+
+describe('Rosdf', () => {
+  it('asdf', () => {
+    return expect(1).toEqual(1);
+  });
+});
