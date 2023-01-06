28d98f311e843a665a3086245a013a3aa1b31c16
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-05 16:20:21 -0800

    refactored server into app and server. app is the actual express app, server sets up listener on 8080 for app

--- src/server/server.js
+++ src/server/server.js
@@ -1,70 +1,7 @@
-const path = require('path');
-const express = require('express');
-const apiRouter = require('./apiRouter.js');
-const authRouter = require('./authRouter.js');
-const passport = require('passport');
-const cors = require('cors');
-
-const session = require('express-session'); // required for oauth session
-const { SESSION_SECRET } = require('../../secrets.js');
-const app = express();
+const { app } = require('./app.js');
 const PORT = 8080;
-
-// session middle parses the oauth jwt
-app.use(
-  session({
-    secret: SESSION_SECRET,
-  })
-);
-app.use(passport.authenticate('session'));
-
-// required for post data
-app.use(express.json());
-app.use(express.urlencoded());
-
-// To get around CORS we normally set response header Access-Control-Allow-Origin --> '*'
-// For example:
-//      app.use((req, res, next) => {
-//        res.header('Access-Control-Allow-Origin', 'http://localhost:5000/');
-//        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//        res.header('Access-Control-Allow-Headers', 'Content-Type');
-//        next();
-//      });
-// However, when ussing cookies many browsers do not allow Access-Control-Allow-Origin '*'
-// Instead, we use the 'cors' npm module.
-app.use(
-  cors({
-    credentials: true,
-    origin: true,
-  })
-);
-
-app.get('/', (req, res) => {
-  return res.status(200).redirect('http://localhost:5000/');
-});
-
-// routers
-app.use('/api', apiRouter);
-app.use('/auth', authRouter);
-
-// page not fuound
-app.use('*', (req, res) => {
-  return res.status(404).send('404 not found');
-});
-
-// global error handler
-app.use((err, req, res, next) => {
-  const errTemplate = {
-    message: 'unknown error occured',
-    status: 500,
-    location: 'unknown location',
-  };
-  return res.status(errObj.status).json(Object.assign(errTemplate, err));
-});
 
 // start server
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
 });
-
-module.exports = app;
