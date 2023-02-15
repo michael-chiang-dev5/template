07fe8fa827a19fb710ecdb30f2d7f0854fd86ce1
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-15 12:07:25 -0500

    Create a slice for questions

--- src/client/Redux/store.js
+++ src/client/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import questionReducer from './slices/questionSlice';
 import userReducer from './slices/userSlice.js';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    questions: questionReducer,
   },
 });
