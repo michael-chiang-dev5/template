60bb58fbcaeacf26717352db73ebdb52d158f729
Bethany Mattern <bethany.a.cable@gmail.com>
2023-02-15 12:08:51 -0500

    Update home and question components

--- src/client/Components/Questions/Questions.jsx
+++ src/client/Components/Questions/Questions.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import axios from 'axios';
+import { setQuestions } from '../../Redux/slices/questionSlice';
 import styles from './questions.module.css';
 
 const Questions = () => {
+
+  const questions = useSelector((state) => state.questions.questions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const response = axios({
+      method: 'get',
+      withCredentials: true,
+      url: 'http://localhost:8080/api/questions'
+    }).then((res) => {
+      console.log(res)
+      if(res.data) {
+        console.log(res.data);
+        dispatch(setQuestions({ questions: res.data }))
+      }
+    }).catch(err => {
+
+    })
+  }, [])
+  // console.log(questions)
+
   return <div>Questions</div>;
 };
 
